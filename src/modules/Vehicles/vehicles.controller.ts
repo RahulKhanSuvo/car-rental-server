6ca5760d9ec91f74f056822ffd79732e7eff4b05
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-12-28 19:52:01 +0600

    added message for no data validation

--- src/modules/Vehicles/vehicles.controller.ts
+++ src/modules/Vehicles/vehicles.controller.ts
@@ -21,7 +21,13 @@ const createVehicle = async (req: Request, res: Response) => {
 const getAllVehicles = async (req: Request, res: Response) => {
     try {
         const result = await vehicleServices.getAllVehicles()
-        res.status(200).json({
+        if (result.rows.length === 0)
+            return res.status(200).json({
+                success: true,
+                message: "No vehicles found",
+                data: []
+            })
+        return res.status(200).json({
             success: true,
             message: "Vehicles retrieved successfully",
             data: result.rows
