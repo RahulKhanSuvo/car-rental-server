9cd24587cd0640bd62827cdf219f5ab69bcf91d6
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2026-01-02 07:22:08 +0600

    feat:added middlewere for create booking

--- src/modules/Bookings/booking.route.ts
+++ src/modules/Bookings/booking.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { bookingController } from "./booking.controller";
+import auth from "../../middleware/auth";
 
 const router = Router()
-router.post("/", bookingController.createBooking)
+router.post("/", auth("admin", "customer"), bookingController.createBooking)
 export const bookingsRouter = router
